af08759c113ce24ae48cb8167ef824d28f1ca4f7
rae <rasyadanafdhalf@gmail.com>
2025-11-27 18:01:35 +0800

    rev_main

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-# none
+from utils import pause, clear, choice
+from auth import login, register, init_csv
+from admin import menu_admin
+from user import menu_user
+from crud_kamar import init_csv_kamar
+from crud_makanan import init_csv_makanan
+
+clear()
+init_csv()
+init_csv_kamar()
+init_csv_makanan()
+print("selamat datang di web pemesanan hotel".upper())
+
+def main():
+    while True:
+        pilihan = [
+            "1. login",
+            "2. register",
+            "3. exit"
+        ]
+
+        menu = choice("", pilihan)
+        clear()
+        print("Kamu memilih: ", menu)
+
+
+        if menu.startswith("1"):
+            username, role = login()
+            if role == "admin":
+                menu_admin()
+            if role == "user":
+                menu_user(username)
+        if menu.startswith("2"):
+            register()
+        if menu.startswith("3"):
+            break
+
+
+main()
